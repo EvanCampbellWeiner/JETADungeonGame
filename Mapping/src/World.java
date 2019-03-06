bd15c533dbc6d64c554394a0fa87ef96c0137cb8
CormanClan <corman.clan3@gmail.com>
2019-03-06 15:46:51 -0500

    Map Generation Code.

---
+++ Mapping/src/World.java
@@ -0,0 +1,19 @@
+public class World {
+    private Floor[] Level;
+    World(int numberOfFloors){
+        this.Level= new Floor[numberOfFloors];
+        for(int loop=0;loop<=numberOfFloors-1;loop++){
+            this.Level[loop]=new Floor(8,8);
+        }
+    }
+
+    public void printWorld(){
+        for(int loop=0;loop<=Level.length-1;loop++){
+            Level[loop].printFloor();
+        }
+    }
+
+    public void customFloor(Floor Floor,int placement) {
+        Level[placement] = Floor;
+    }
+}

dfc2d6c3baebe60290cee793dc86d2ace6a62ef1
Evan Campbell Weiner <evancampbellweiner@gmail.com>
2019-04-05 19:55:43 -0400

    Compiled final project to be submitted

--- Entity/src/JUnitTest.java
+++ Entity/src/JUnitTest.java
@@ -2,18 +2,21 @@
 //import org.junit.Test;
 //import static org.junit.Assert.*;
 // or we can simply: import Money.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Random;
 
 
 public class JUnitTest {
 
-/*    @Test
+   @Test
     public void FloorTest() throws Exception {
         Floor MyHouse = new Floor(8, 8);
 
         Tile ShouldWork= MyHouse.GetTile(-2,-2);
         assertNotNull(ShouldWork);
     }
-    */
+
 }
 
 
